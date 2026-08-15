3038ec0c3036d13b8a0c5b7bf928e997008ac056
Mukul <mukulkashyap.business@gmail.com>
2026-08-16 00:07:40 +0530

    add condition in home handler

--- main.go
+++ main.go
@@ -6,6 +6,11 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
 	w.Write([]byte("Hello"))
 }
 
@@ -22,6 +27,7 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
+
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
